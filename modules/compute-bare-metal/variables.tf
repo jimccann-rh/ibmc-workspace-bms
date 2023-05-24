c45a19f2c8d160b59cef05624c5d036aedb84e1e
jimb0 <jimccann@redhat.com>
2023-05-24 09:47:55 -0400

    change 25g

--- modules/compute-bare-metal/variables.tf
+++ modules/compute-bare-metal/variables.tf
@@ -7,5 +7,6 @@ variable "domain_name" {}
 variable "network_speed" {
   description = "The network speed for the node instance."
   type        = number
-  default     = 10000
+  default     = 25000
+  #default     = 10000
 }
